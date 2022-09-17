301e0527630a81cc2d8d53ab70d3ef22ccaacec0
DndServ2022 <intern@tadeco-group.tn>
2022-09-17 14:51:56 +0100

    styling hot-patch

--- src/view/Profile/Suggestions/Suggest.tsx
+++ src/view/Profile/Suggestions/Suggest.tsx
@@ -85,257 +85,254 @@ export default function Suggest() {
   };
 
   return (
-    <div className='app__profile'>
-      <div className='profile'>
-        <div className='mainFlex'>
-          <Link to='/profile' className='backTigger'>
-            <i className='fa-solid fa-arrow-left' />
-            <span>Retour</span>
-          </Link>
-          <section className='suggestionsPanel'>
-            <div className='mainContent'>
-              <Toaster />
-              <div className='archieve__header'>
-                <h2>Suggérer&nbsp;DU&nbsp;contenu</h2>
-                <div className='communiquer__bar'></div>
-              </div>
-
-              <form onSubmit={handleSubmit(handleFormSubmission)}>
-                {/* DESCRIPTION */}
-                <section className='description'>
-                  Description lorem ipsum, dolor sit amet consectetur
-                  adipisicing elit. Omnis voluptates deserunt optio maxime ad
-                  vero voluptatum quibusdam laborum nulla architecto recusandae
-                  nam, nihil quos. Rerum incidunt tenetur accusantium rem
-                  voluptates, voluptas iste nihil, ipsam
-                </section>
-
-                {/* CATEGORIES */}
-                <section className='contentCategory'>
-                  <span>Catégorie</span>
-
-                  <select name='categories' ref={register}>
-                    <option value=''>Select category...</option>
-                    <option value='manifestation'>Manifestation</option>
-                    <option value='article scientifique'>
-                      article scientifique
-                    </option>
-                    <option value='article journalistique'>
-                      article journalistique
-                    </option>
-                    <option value='rapport'>rapport</option>
-                    <option value='entretien'>entretien</option>
-                    <option value='données numériques'>
-                      données numériques
-                    </option>
-                    <option value='autre'>autre</option>
-                  </select>
-                </section>
-
-                {/* CONTENT TYPE */}
-                <section className='contentTypes'>
-                  <span>Type de contenu</span>
-
-                  <Splide
-                    className='splide'
-                    options={{
-                      gap: ".2rem",
-                      perPage: 5,
-                      pagination: false,
-                      Wheel: true,
-                      width: "100%",
-                    }}>
-                    <SplideSlide>
-                      <label htmlFor='Text'>
-                        <input
-                          type='radio'
-                          ref={register}
-                          value='Texte'
-                          name='contentTypeRadio'
-                          className='contentTypeRadio'
-                          id='Text'
-                        />
-                        <div className='TypeCard'>
-                          <GrTextAlignFull className='icon' />
-                          <span>Text</span>
-                        </div>
-                      </label>
-                    </SplideSlide>
-
-                    <SplideSlide>
-                      <label htmlFor='Audio'>
-                        <input
-                          type='radio'
-                          ref={register}
-                          value='audio'
-                          name='contentTypeRadio'
-                          className='contentTypeRadio'
-                          id='Audio'
-                        />
-                        <div className='TypeCard'>
-                          <BiMicrophone className='icon' />
-                          <span>Audio</span>
-                        </div>
-                      </label>
-                    </SplideSlide>
-
-                    <SplideSlide>
-                      <label htmlFor='Video'>
-                        <input
-                          type='radio'
-                          ref={register}
-                          value='vidéo'
-                          name='contentTypeRadio'
-                          className='contentTypeRadio'
-                          id='Video'
-                        />
-                        <div className='TypeCard'>
-                          <IoVideocamOutline className='icon' />
-                          <span>Video</span>
-                        </div>
-                      </label>
-                    </SplideSlide>
-
-                    <SplideSlide>
-                      <label htmlFor='Lien'>
-                        <input
-                          type='radio'
-                          ref={register}
-                          value='lien web'
-                          name='contentTypeRadio'
-                          className='contentTypeRadio'
-                          id='Lien'
-                        />
-                        <div className='TypeCard'>
-                          <CgLink className='icon' />
-                          <span>Lien</span>
-                        </div>
-                      </label>
-                    </SplideSlide>
-
-                    <SplideSlide>
-                      <label htmlFor='Photo'>
-                        <input
-                          type='radio'
-                          ref={register}
-                          value='photo'
-                          name='contentTypeRadio'
-                          className='contentTypeRadio'
-                          id='Photo'
-                        />
-                        <div className='TypeCard'>
-                          <BsCamera className='icon' />
-                          <span>Photo</span>
-                        </div>
-                      </label>
-                    </SplideSlide>
-
-                    <SplideSlide>
-                      <label htmlFor='Infographie'>
-                        <input
-                          type='radio'
-                          ref={register}
-                          value='infographie'
-                          name='contentTypeRadio'
-                          className='contentTypeRadio'
-                          id='Infographie'
-                        />
-                        <div className='TypeCard'>
-                          <VscGraphScatter className='icon' />
-                          <span>Infographie</span>
-                        </div>
-                      </label>
-                    </SplideSlide>
-
-                    <SplideSlide>
-                      <label htmlFor='Statistiques'>
-                        <input
-                          type='radio'
-                          ref={register}
-                          value='statistiques'
-                          name='contentTypeRadio'
-                          className='contentTypeRadio'
-                          id='Statistiques'
-                        />
-                        <div className='TypeCard'>
-                          <BiStats className='icon' />
-                          <span>Statistiques</span>
-                        </div>
-                      </label>
-                    </SplideSlide>
-
-                    <SplideSlide>
-                      <label htmlFor='Autre'>
-                        <input
-                          type='radio'
-                          ref={register}
-                          value='autre'
-                          name='contentTypeRadio'
-                          className='contentTypeRadio'
-                          id='Autre'
-                        />
-                        <div className='TypeCard'>
-                          <AiOutlineQuestion className='icon' />
-                          <span>Autre</span>
-                        </div>
-                      </label>
-                    </SplideSlide>
-                  </Splide>
-                </section>
-
-                {/* SUPPORT (Audio, Video, Photo)*/}
-                <section className='support'>
-                  <span>Support</span>
-
-                  <div className='dragAndDropAreaFlex' {...getRootProps()}>
-                    <div className='DADControls'>
-                      <CgSoftwareUpload className='icon' />
-                      {isDragActive ? (
-                        <span>Drop here to upload</span>
-                      ) : (
-                        <div>
-                          <span>Drop files here</span>
-                          <span>or</span>
-                          <label className='customFileUpload'>
-                            <input
-                              id='file-upload'
-                              // type="file"
-                              // name="support"
-                              // ref={register}
-                              {...getInputProps()}
-                            />
-                            Select files
-                          </label>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </section>
-
-                {/* DESCRIPTION (Text, Link)*/}
-                <section className='description'>
-                  <span>Description</span>
-                  <textarea
-                    cols={30}
-                    rows={10}
-                    placeholder='Description....'
-                    name='description'
-                    ref={register}></textarea>
-                  {errors.description && (
-                    <FormError message={errors.description?.message} />
-                  )}
-                </section>
-
-                {/* ACTION BUTTONS */}
-                <section className='formActions'>
-                  <input type='reset' value='Annuler' />
-                  <input type='submit' value='Valider' />
-                </section>
-              </form>
-            </div>
-          </section>
+    <div className='app__contenu'>
+      <Link to='/profile'>
+        <div className='retour'>
+          <i className='fa-solid fa-arrow-left'></i> Retour
         </div>
-      </div>
+      </Link>
+      <section className='contenu'>
+        <div className='mainContent'>
+          <Toaster />
+          <div className='archieve__header'>
+            <h2>Suggérer&nbsp;DU&nbsp;contenu</h2>
+            <div className='communiquer__bar'></div>
+          </div>
+
+          <form onSubmit={handleSubmit(handleFormSubmission)}>
+            {/* DESCRIPTION */}
+            <section className='description'>
+              Description lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Omnis voluptates deserunt optio maxime ad
+              vero voluptatum quibusdam laborum nulla architecto recusandae
+              nam, nihil quos. Rerum incidunt tenetur accusantium rem
+              voluptates, voluptas iste nihil, ipsam
+            </section>
+
+            {/* CATEGORIES */}
+            <section className='contentCategory'>
+              <span>Catégorie</span>
+
+              <select name='categories' ref={register}>
+                <option value=''>Select category...</option>
+                <option value='manifestation'>Manifestation</option>
+                <option value='article scientifique'>
+                  article scientifique
+                </option>
+                <option value='article journalistique'>
+                  article journalistique
+                </option>
+                <option value='rapport'>rapport</option>
+                <option value='entretien'>entretien</option>
+                <option value='données numériques'>
+                  données numériques
+                </option>
+                <option value='autre'>autre</option>
+              </select>
+            </section>
+
+            {/* CONTENT TYPE */}
+            <section className='contentTypes'>
+              <span>Type de contenu</span>
+
+              <Splide
+                className='splide'
+                options={{
+                  gap: ".2rem",
+                  perPage: 5,
+                  pagination: false,
+                  Wheel: true,
+                  width: "100%",
+                }}>
+                <SplideSlide>
+                  <label htmlFor='Text'>
+                    <input
+                      type='radio'
+                      ref={register}
+                      value='Texte'
+                      name='contentTypeRadio'
+                      className='contentTypeRadio'
+                      id='Text'
+                    />
+                    <div className='TypeCard'>
+                      <GrTextAlignFull className='icon' />
+                      <span>Text</span>
+                    </div>
+                  </label>
+                </SplideSlide>
+
+                <SplideSlide>
+                  <label htmlFor='Audio'>
+                    <input
+                      type='radio'
+                      ref={register}
+                      value='audio'
+                      name='contentTypeRadio'
+                      className='contentTypeRadio'
+                      id='Audio'
+                    />
+                    <div className='TypeCard'>
+                      <BiMicrophone className='icon' />
+                      <span>Audio</span>
+                    </div>
+                  </label>
+                </SplideSlide>
+
+                <SplideSlide>
+                  <label htmlFor='Video'>
+                    <input
+                      type='radio'
+                      ref={register}
+                      value='vidéo'
+                      name='contentTypeRadio'
+                      className='contentTypeRadio'
+                      id='Video'
+                    />
+                    <div className='TypeCard'>
+                      <IoVideocamOutline className='icon' />
+                      <span>Video</span>
+                    </div>
+                  </label>
+                </SplideSlide>
+
+                <SplideSlide>
+                  <label htmlFor='Lien'>
+                    <input
+                      type='radio'
+                      ref={register}
+                      value='lien web'
+                      name='contentTypeRadio'
+                      className='contentTypeRadio'
+                      id='Lien'
+                    />
+                    <div className='TypeCard'>
+                      <CgLink className='icon' />
+                      <span>Lien</span>
+                    </div>
+                  </label>
+                </SplideSlide>
+
+                <SplideSlide>
+                  <label htmlFor='Photo'>
+                    <input
+                      type='radio'
+                      ref={register}
+                      value='photo'
+                      name='contentTypeRadio'
+                      className='contentTypeRadio'
+                      id='Photo'
+                    />
+                    <div className='TypeCard'>
+                      <BsCamera className='icon' />
+                      <span>Photo</span>
+                    </div>
+                  </label>
+                </SplideSlide>
+
+                <SplideSlide>
+                  <label htmlFor='Infographie'>
+                    <input
+                      type='radio'
+                      ref={register}
+                      value='infographie'
+                      name='contentTypeRadio'
+                      className='contentTypeRadio'
+                      id='Infographie'
+                    />
+                    <div className='TypeCard'>
+                      <VscGraphScatter className='icon' />
+                      <span>Infographie</span>
+                    </div>
+                  </label>
+                </SplideSlide>
+
+                <SplideSlide>
+                  <label htmlFor='Statistiques'>
+                    <input
+                      type='radio'
+                      ref={register}
+                      value='statistiques'
+                      name='contentTypeRadio'
+                      className='contentTypeRadio'
+                      id='Statistiques'
+                    />
+                    <div className='TypeCard'>
+                      <BiStats className='icon' />
+                      <span>Statistiques</span>
+                    </div>
+                  </label>
+                </SplideSlide>
+
+                <SplideSlide>
+                  <label htmlFor='Autre'>
+                    <input
+                      type='radio'
+                      ref={register}
+                      value='autre'
+                      name='contentTypeRadio'
+                      className='contentTypeRadio'
+                      id='Autre'
+                    />
+                    <div className='TypeCard'>
+                      <AiOutlineQuestion className='icon' />
+                      <span>Autre</span>
+                    </div>
+                  </label>
+                </SplideSlide>
+              </Splide>
+            </section>
+
+            {/* SUPPORT (Audio, Video, Photo)*/}
+            <section className='support'>
+              <span>Support</span>
+
+              <div className='dragAndDropAreaFlex' {...getRootProps()}>
+                <div className='DADControls'>
+                  <CgSoftwareUpload className='icon' />
+                  {isDragActive ? (
+                    <span>Drop here to upload</span>
+                  ) : (
+                    <div>
+                      <span>Drop files here</span>
+                      <span>or</span>
+                      <label className='customFileUpload'>
+                        <input
+                          id='file-upload'
+                          // type="file"
+                          // name="support"
+                          // ref={register}
+                          {...getInputProps()}
+                        />
+                        Select files
+                      </label>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </section>
+
+            {/* DESCRIPTION (Text, Link)*/}
+            <section className='description'>
+              <span>Description</span>
+              <textarea
+                cols={30}
+                rows={10}
+                placeholder='Description....'
+                name='description'
+                ref={register}></textarea>
+              {errors.description && (
+                <FormError message={errors.description?.message} />
+              )}
+            </section>
+
+            {/* ACTION BUTTONS */}
+            <section className='formActions'>
+              <input type='reset' value='Annuler' />
+              <input type='submit' value='Valider' />
+            </section>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
