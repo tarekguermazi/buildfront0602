ed51fe3a5c30a32d99b17dfefe7a0099fbe9b059
DndServ2022 <intern@tadeco-group.tn>
2022-09-27 17:28:40 +0100

    Mediatheque main view placeholder

--- src/view/Médiathèque/Mediatheque.tsx
+++ src/view/Médiathèque/Mediatheque.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+// COMPONENTS
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import MegaFooter from "../Layout/MegaFooter";
+import Footer from "../Layout/Footer";
 
 function Mediatheque() {
-  return <h1>Mediatheque</h1>;
+  return (
+    <section>
+
+      <div className='app__contenu'>
+        <section className='wideContent'>
+          <Header />
+          <HeroSection />
+          {/* MAIN GRID... */}
+        </section>
+      </div>
+      <MegaFooter />
+      <Footer />
+    </section>
+  );
 }
 
 export default Mediatheque;
