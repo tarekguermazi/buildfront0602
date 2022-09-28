6fb898c0345542142fcced69481aa8cc260dbc54
Mootassem <mootassame@gmail.com>
2022-09-28 11:26:31 -0400

    add the detaill header in the publication page

--- src/view/Publications/Publications.tsx
+++ src/view/Publications/Publications.tsx
@@ -4,8 +4,7 @@ import { i18n } from "../../i18n";
 
 function Publications() {
   return (
-    <div className='app__publication'>
-      {" "}
+    <div className='publication__page'>
       <Breadcrumb
         title={i18n("entities.publication.label")}
         items={[
@@ -13,6 +12,87 @@ function Publications() {
           [i18n("entities.publication.label")],
         ]}
       />
+      <div className='app__pub'>
+        <div className='publication__header'>
+          <div className='image__pub'>
+            <div className='pub__relative'>
+              <img src='https://placehold.jp/570x390.png' alt='Image' />
+              <div className='__detaills'>
+                <div className='list__detaill'>
+                  <div className='detaill__header'>
+                    <div className='__left'>
+                      <p>Migration</p>
+                    </div>
+                    <div className='__right'>10h32</div>
+                  </div>
+                  <div className='__content'>
+                    Plus de 13500 migrants clandestins tunisiens sont arrivés en
+                    Italie
+                  </div>
+                  <div className='sub__content'>
+                    Cras eleifend mattis lectus, sit amet commodo justo sagittis
+                    id. Maecenas non fringilla ipsum. Nunc varius vitae sem nec
+                    finibus. Duis mattis nulla dui...
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='pub__relative'>
+              <img src='https://placehold.jp/269x390.png' alt='' />
+              <div className='__detaills'>
+                <div className='list__detaill'>
+                  <div className='detaill__header'>
+                    <div className='__left'>
+                      <p>Migration</p>
+                    </div>
+                    <div className='__right'>10h32</div>
+                  </div>
+                  <div className='__content'>
+                    Plus de 13500 migrants clandestins tunisiens sont arrivés en
+                    Italie
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='last__pub'>
+              <div className='pub__relative'>
+                <img src='https://placehold.jp/272x182.png' alt='' />
+                <div className='__detaills'>
+                  <div className='list__detaill'>
+                    <div className='detaill__header'>
+                      <div className='__left'>
+                        <p>Migration</p>
+                      </div>
+                      <div className='__right'>10h32</div>
+                    </div>
+                    <div className='__content'>
+                      Plus de 13500 migrants clandestins tunisiens sont arrivés
+                      en Italie
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='pub__relative'>
+                <img src='https://placehold.jp/272x182.png' alt='' />
+                <div className='__detaills'>
+                  <div className='list__detaill'>
+                    <div className='detaill__header'>
+                      <div className='__left'>
+                        <p>Migration</p>
+                      </div>
+                      <div className='__right'>10h32</div>
+                    </div>
+                    <div className='__content'>
+                      Plus de 13500 migrants clandestins tunisiens sont arrivés
+                      en Italie
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
