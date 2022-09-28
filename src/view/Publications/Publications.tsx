be8671c2380a4ac5b5b591cf83149ff915145719
Mootassem <mootassame@gmail.com>
2022-09-28 10:23:55 -0400

    styling the voir plus page for publication

--- src/view/Publications/Publications.tsx
+++ src/view/Publications/Publications.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Breadcrumb from "src/view/shared/Breadcrumb";
+import { i18n } from "../../i18n";
 
 function Publications() {
-  return <div>Publications</div>;
+  return (
+    <div className='app__publication'>
+      {" "}
+      <Breadcrumb
+        title={i18n("entities.publication.label")}
+        items={[
+          [i18n("dashboard.menu"), "/"],
+          [i18n("entities.publication.label")],
+        ]}
+      />
+    </div>
+  );
 }
 
 export default Publications;
