a4f4e52f03815c692652b4900ebf00ee294110c9
DndServ2022 <intern@tadeco-group.tn>
2022-09-30 16:47:03 +0100

    sections spacing + switched classesNames

--- src/view/Médiathèque/styles/MainGridLayout.tsx
+++ src/view/Médiathèque/styles/MainGridLayout.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 // COMPONENTS
 import NewsLetterWidget from 'src/view/shared/NewsLetterWidget';
+import Videos from '../Videos';
 
 // ICONS/Assets
 import { envelope } from "src/assets/images";
@@ -10,12 +11,12 @@ import { envelope } from "src/assets/images";
 export default function MainGridLayout() {
     return (
         <MainContentLayout>
-            <section className='mainContentRightSection'>
-                <h1>videos</h1>
+            <section className='mainContentLeftSection'>
+                <Videos />
                 <h2>photos</h2>
             </section>
             {/* LEFT SECTION */}
-            <section className='mainContentLeftSection'>
+            <section className='mainContentRightSection'>
                 <h2>Documentaires</h2>
                 {/* NEWS LETTER COMPONENT */}
                 <NewsLetterWidget envelope={envelope} layout='minimal' />
@@ -39,12 +40,12 @@ const MainContentLayout = styled.section`
     justify-content: space-between;
 
     .mainContentRightSection{
-        background-color: red;
-        width: 100%;
-    }
-
-    .mainContentLeftSection{
         background-color: pink;
         width: 350px !important;
+        margin-left: 2rem;
+    }
+    
+    .mainContentLeftSection{
+        width: 100%;
     }
 `;
