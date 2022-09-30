55fd29d5453a667aa94f1663833c0182101e4e70
DndServ2022 <intern@tadeco-group.tn>
2022-09-30 14:53:23 +0100

    mainContent layout placeholder

---
+++ src/view/Médiathèque/styles/MainGridLayout.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function MainGridLayout() {
+    return (
+        <MainContentLayout>
+            <section className='mainContentRightSection'>
+                <h1>videos</h1>
+                <h2>photos</h2>
+            </section>
+            {/* LEFT SECTION */}
+            <section className='mainContentLeftSection'>
+                <h2>Documentaires</h2>
+                <h2>NewsLetter</h2>
+                <h2>Podcasts</h2>
+            </section>
+        </MainContentLayout>
+    )
+}
+
+const MainContentLayout = styled.section`
+    margin: auto;
+    width: var(--cerntered-content);
+    min-height: 200px;
+    background-color: yellow;
+    position: relative;
+    top: -180px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+
+    .mainContentRightSection{
+        background-color: red;
+        width: 100%;
+    }
+
+    .mainContentLeftSection{
+        background-color: pink;
+        width: 350px !important;
+    }
+`;
