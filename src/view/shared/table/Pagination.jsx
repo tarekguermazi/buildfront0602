22f8b3b184aa5d2022bffe5eee208e9f296b9c72
Mootassem <mootassame@gmail.com>
2022-10-04 05:24:48 -0400

    show the pagination text

--- src/view/shared/table/Pagination.jsx
+++ src/view/shared/table/Pagination.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import RCPagination from "rc-pagination";
 import PaginationWrapper from "src/view/shared/table/PaginationWrapper";
 import PropTypes from "prop-types";
@@ -10,6 +10,13 @@ const Pagination = (props) => {
       pageSize: Number(pageSize),
     });
   };
+
+  useEffect(() => {
+    const NextButton = document.querySelector('[aria-label="next page"]');
+    NextButton.innerHTML = "Suivant";
+    const PrevButton = document.querySelector('[aria-label="prev page"]');
+    PrevButton.innerHTML = "Precedent";
+  }, []);
 
   const { pagination, disabled, showTotal } = props;
   const { current, pageSize, total } = pagination;
