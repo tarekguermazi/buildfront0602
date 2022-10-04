2782ab0e353d4dba7ec9ab512f78236949798ee0
DndServ2022 <intern@tadeco-group.tn>
2022-10-04 14:06:48 +0100

    unused icons

--- src/view/Médiathèque/NewContent.tsx
+++ src/view/Médiathèque/NewContent.tsx
@@ -10,11 +10,8 @@ import '@splidejs/react-splide/css';
 // ICONS
 import { GrTextAlignFull } from "react-icons/gr";
 import { IoVideocamOutline } from "react-icons/io5";
-import { CgLink } from "react-icons/cg";
 import { BsCamera } from "react-icons/bs";
-import { BiMicrophone, BiStats } from "react-icons/bi";
-import { AiOutlineQuestion } from "react-icons/ai";
-import { VscGraphScatter } from "react-icons/vsc";
+import { BiMicrophone } from "react-icons/bi";
 import moment from 'moment';
 
 
