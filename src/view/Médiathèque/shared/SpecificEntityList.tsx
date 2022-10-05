f25a2f54c5599db9dc497639c596a523ce7aafa1
DndServ2022 <intern@tadeco-group.tn>
2022-10-05 14:26:24 +0100

    using params to get entity type

--- src/view/Médiathèque/shared/SpecificEntityList.tsx
+++ src/view/Médiathèque/shared/SpecificEntityList.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function SpecificEntityList() {
+    let params = useParams();
     return (
-        <div>SpecificEntityList</div>
+        <section>
+
+        </section>
     )
 }
