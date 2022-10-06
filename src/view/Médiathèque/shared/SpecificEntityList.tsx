f606808238e81a0cf7284dbde8256efd363237b3
aymkh <aymen4real@gmail.com>
2022-10-06 09:05:23 +0100

    content-layout

--- src/view/Médiathèque/shared/SpecificEntityList.tsx
+++ src/view/Médiathèque/shared/SpecificEntityList.tsx
@@ -1,11 +1,57 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
+import "react-loading-skeleton/dist/skeleton.css";
+
+
+// COMPONENTS
+import BreadCrumbs from 'src/view/shared/BreadCrumbs';
+import Skeleton from 'react-loading-skeleton';
 
 export default function SpecificEntityList() {
-    let params = useParams();
+    const params = useParams();
+    const param = params.entity_type;
+
     return (
         <section>
+            <div className='app__contenu'>
+                <section className='wideContent'>
+                    <h1>{param}</h1>
+                    <BreadCrumbs view={"Médiathèques / " + param} />
 
+                    <MainLoadingLayout>
+                        <Skeleton height={300} width={270} />
+                        <Skeleton height={300} width={270} />
+                        <Skeleton height={300} width={270} />
+                        <Skeleton height={300} width={270} />
+                    </MainLoadingLayout>
+
+                    <MainLayout>
+
+                    </MainLayout>
+                </section>
+            </div>
         </section>
     )
 }
+
+
+const MainLoadingLayout = styled.section`
+    width: 100%;
+    margin: 2rem 0;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    .react-loading-skeleton{
+        width: 100%;
+        height: 200px;
+    }
+`;
+
+const MainLayout = styled.section`
+    width: 100%;
+    margin: 2rem 0;
+`;
