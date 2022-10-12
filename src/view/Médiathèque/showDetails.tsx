1aa1afe8350222b1276308c807610a472fd49c0e
aymkh <aymen4real@gmail.com>
2022-10-12 09:04:36 +0100

    🗑️ static array of image was used for testing

--- src/view/Médiathèque/showDetails.tsx
+++ src/view/Médiathèque/showDetails.tsx
@@ -52,21 +52,6 @@ export default function ShowPublication() {
         return (d?.split('T')[1]);
     }
 
-    const images = [
-        {
-            original: 'https://picsum.photos/id/1018/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1018/250/150/',
-        },
-        {
-            original: 'https://picsum.photos/id/1015/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1015/250/150/',
-        },
-        {
-            original: 'https://picsum.photos/id/1019/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1019/250/150/',
-        },
-    ];
-
 
     return (
         <section>
