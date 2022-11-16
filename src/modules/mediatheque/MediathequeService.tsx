a0dc884dd4872269d617ab0b7ba0768d93add406
aymkh <aymen4real@gmail.com>
2022-11-16 11:01:20 +0100

    tenantID fix

--- src/modules/mediatheque/MediathequeService.tsx
+++ src/modules/mediatheque/MediathequeService.tsx
@@ -5,22 +5,22 @@ import moment from 'moment';
 export default class MediathequeService {
     // A LA UNE (latest 10)
     static async getLatestMediatheques() {
-        // const tenantId = AuthCurrentTenant.get();
-        const tenantId = '6315dbfd1241d0001e764e26';
+        const tenantId = AuthCurrentTenant.get();
+        // const tenantId = '6315dbfd1241d0001e764e26';
         const response = await authAxios.get(`/tenant/${tenantId}/mediatique/`);
         return response.data;
     }
 
     static async getMediathequesBasedOnType(extension) {
-        // const tenantId = AuthCurrentTenant.get();
-        const tenantId = '6315dbfd1241d0001e764e26';
+        const tenantId = AuthCurrentTenant.get();
+        // const tenantId = '6315dbfd1241d0001e764e26';
         const response = await authAxios.get(`/tenant/${tenantId}/mediatique/${extension}`);
         return response.data;
     }
 
     static async getOneMediatheque(id) {
-        // const tenantId = AuthCurrentTenant.get();
-        const tenantId = '6315dbfd1241d0001e764e26';
+        const tenantId = AuthCurrentTenant.get();
+        // const tenantId = '6315dbfd1241d0001e764e26';
         const response = await authAxios.get(`/tenant/${tenantId}/mediatique/${id}`);
         return response.data;
     }
