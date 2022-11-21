623b0b01ce1a3cf87b919a77d84c194a1733fc56
DndServ2022 <112878546+DndServ2022@users.noreply.github.com>
2022-11-21 11:28:58 +0100

    TenantID switched back to dynamic (had o set it t static for testing)

--- src/modules/Search/SearchService.tsx
+++ src/modules/Search/SearchService.tsx
@@ -4,16 +4,14 @@ import AuthCurrentTenant from "src/modules/auth/authCurrentTenant";
 export default class SearchService {
 
     static async getSearchResultsForPublicationsBasedOnSearchString(SEARCH_STRING, FILTER_STRING) {
-        // const tenantId = AuthCurrentTenant.get();
-        const tenantId = '6368fcb72fdd40001e4da469';
+        const tenantId = AuthCurrentTenant.get();
         const response = await authAxios.get(`/tenant/${tenantId}/publication?filter[descriptionFR]=${SEARCH_STRING}${FILTER_STRING}`);
         return response.data;
     }
 
     // ARCHIVE SEARCH
     static async getSearchResultsForArchiveBasedOnSearchString(SEARCH_STRING, FILTER_STRING) {
-        // const tenantId = AuthCurrentTenant.get();
-        const tenantId = '6368fcb72fdd40001e4da469';
+        const tenantId = AuthCurrentTenant.get();
         const response = await authAxios.get(`/tenant/${tenantId}/publication?filter[descriptionFR]=${SEARCH_STRING}&filter[statut]=archive${FILTER_STRING}`);
         return response.data;
     }
