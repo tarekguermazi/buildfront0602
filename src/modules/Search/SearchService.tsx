05a9687df10551d49fc4e53ae442c89fb0e1653e
DndServ2022 <112878546+DndServ2022@users.noreply.github.com>
2022-11-21 11:27:02 +0100

    Archive search bar gets publication of status=archived

--- src/modules/Search/SearchService.tsx
+++ src/modules/Search/SearchService.tsx
@@ -9,4 +9,12 @@ export default class SearchService {
         const response = await authAxios.get(`/tenant/${tenantId}/publication?filter[descriptionFR]=${SEARCH_STRING}${FILTER_STRING}`);
         return response.data;
     }
+
+    // ARCHIVE SEARCH
+    static async getSearchResultsForArchiveBasedOnSearchString(SEARCH_STRING, FILTER_STRING) {
+        // const tenantId = AuthCurrentTenant.get();
+        const tenantId = '6368fcb72fdd40001e4da469';
+        const response = await authAxios.get(`/tenant/${tenantId}/publication?filter[descriptionFR]=${SEARCH_STRING}&filter[statut]=archive${FILTER_STRING}`);
+        return response.data;
+    }
 }
--- src/view/Search/SearchHeader.tsx
+++ src/view/Search/SearchHeader.tsx
@@ -38,7 +38,7 @@ export default function SearchHeader({ location }) {
     // ARCHVIE
     const searchArchive = (SEARCH_STRING: string, FILTER_STRING: string) => {
         setIsLoading(true);
-        SearchService.getSearchResultsForPublicationsBasedOnSearchString(SEARCH_STRING, '')
+        SearchService.getSearchResultsForArchiveBasedOnSearchString(SEARCH_STRING, FILTER_STRING)
             .then(res => {
                 setSRP(SRP => SRP.concat(res));
                 setIsLoading(false);
