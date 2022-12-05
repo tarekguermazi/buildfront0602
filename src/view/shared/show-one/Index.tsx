7614861a7beea4961ee6a6250cac6dd97378e1c3
Mootassem <mootassame@gmail.com>
2022-12-05 11:51:58 -0500

    show the title and the date in the detaillPublication

--- src/view/shared/show-one/Index.tsx
+++ src/view/shared/show-one/Index.tsx
@@ -1,63 +1,61 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 // SERVICES
-import PublicationService from 'src/modules/publication/publicationService';
+import PublicationService from "src/modules/publication/publicationService";
 // COMPONENTS
-import Header from './components/Header';
-import Main from './components/Main';
-import RelatedContent from './RelatedContent';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import RelatedContent from "./RelatedContent";
 
 export default function Index() {
-    // STATES AND VARS
-    let ID = useParams().id;
-    const [PublicationDetails, setPublicationDetails] = useState({});
-    const [data, setData] = useState([]);
+  // STATES AND VARS
+  let ID = useParams().id;
+  const [PublicationDetails, setPublicationDetails] = useState({});
+  const [data, setData] = useState([]);
 
+  // Get the details of that one publication
+  const getPublicationDetails = (PUBLICATION_ID) => {
+    PublicationService.find(PUBLICATION_ID)
+      .then((response: any) => {
+        setPublicationDetails(response);
+        // loadig photos to be used in the grid
+        if (response.photos.length >= 1) {
+          response.photos.map((p) => {
+            setData((data) => data.concat(p.downloadUrl));
+          });
+        }
+        if (response.supports.length >= 1) {
+          response.supports.map((supp) => {
+            setData((data) => data.concat(supp.downloadUrl));
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  // FETCH ON LOAD
+  useEffect(() => {
+    getPublicationDetails(ID);
+  }, []);
 
-    // Get the details of that one publication
-    const getPublicationDetails = PUBLICATION_ID => {
-        PublicationService.find(PUBLICATION_ID)
-            .then((response: any) => {
-                setPublicationDetails(response);
-                // loadig photos to be used in the grid
-                if (response.photos.length >= 1) {
-                    response.photos.map((p) => {
-                        setData((data) => data.concat(p.downloadUrl));
-                    });
-                }
-                if (response.supports.length >= 1) {
-                    response.supports.map((supp) => {
-                        setData((data) => data.concat(supp.downloadUrl));
-                    });
-                }
-            })
-            .catch(err => { console.error(err) })
-    }
-    // FETCH ON LOAD
-    useEffect(() => {
-        getPublicationDetails(ID);
-    }, []);
-
-
-
-    // TEMPLATE
-    return (
-        <MainLayout>
-            <Header
-                PUBLICATION_TITLE={PublicationDetails['title'] ?? 'N.A'}
-                PUBLICATION_CREATED_AT={PublicationDetails['createdAt'] ?? 'N.A'}
-                PUBLICATION_TYPE={PublicationDetails['thematique']?.titleFR ?? 'N.A'}
-            />
-            <Main PUBLICATION={PublicationDetails} data={data} />
-            <RelatedContent type={PublicationDetails['type'] ?? 'N.A'} />
-        </MainLayout>
-    )
+  // TEMPLATE
+  return (
+    <MainLayout>
+      <Header
+        PUBLICATION_TITLE={PublicationDetails ?? "N.A"}
+        PUBLICATION_CREATED_AT={PublicationDetails["updatedAt"] ?? "N.A"}
+        PUBLICATION_TYPE={PublicationDetails["thematique"] ?? "N.A"}
+      />
+      <Main PUBLICATION={PublicationDetails} data={data} />
+      <RelatedContent type={PublicationDetails["type"] ?? "N.A"} />
+    </MainLayout>
+  );
 }
 
-
 const MainLayout = styled.section`
-    width: 1170px;
-    height: 100%;
-    margin: 1rem auto;
+  width: 1170px;
+  height: 100%;
+  margin: 1rem auto;
 `;
--- src/view/shared/show-one/components/Header.tsx
+++ src/view/shared/show-one/components/Header.tsx
@@ -1,71 +1,87 @@
-import React from 'react'
-import styled from 'styled-components'
-import PublicationService from 'src/modules/publication/publicationService'
+import React from "react";
+import styled from "styled-components";
+import PublicationService from "src/modules/publication/publicationService";
 // ICONS
-import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs'
+import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
+import Translate from "../../Translate";
 
-export default function Header({ PUBLICATION_TITLE, PUBLICATION_CREATED_AT, PUBLICATION_TYPE }) {
-    return (
-        <HeaderLayout>
-            <span className="categoryBadge">{PUBLICATION_TYPE}</span>
+export default function Header({
+  PUBLICATION_TITLE,
+  PUBLICATION_CREATED_AT,
+  PUBLICATION_TYPE,
+}) {
+  return (
+    <HeaderLayout>
+      <span className='categoryBadge'>
+        {Translate.Trans("title", PUBLICATION_TYPE)}
+      </span>
 
-            <h1>{PUBLICATION_TITLE}</h1>
+      <h1>{Translate.Trans("title", PUBLICATION_TITLE)}</h1>
 
-            <div className="data">
-                <div className="dateAndOwner">
-                    <span>Publié le {PublicationService.pipeDate(PUBLICATION_CREATED_AT)}</span>
-                </div>
-                <div className="socials">
-                    <span>Partager :</span>
-                    <button><BsFacebook /></button>
-                    <button><BsTwitter /></button>
-                    <button><BsInstagram /></button>
-                    <button><BsLinkedin /></button>
-                </div>
-            </div>
-        </HeaderLayout>
-    )
+      <div className='data'>
+        <div className='dateAndOwner'>
+          <span>
+            Publié le {PublicationService.pipeDate(PUBLICATION_CREATED_AT)}
+          </span>
+        </div>
+        <div className='socials'>
+          <span>Partager :</span>
+          <button>
+            <BsFacebook />
+          </button>
+          <button>
+            <BsTwitter />
+          </button>
+          <button>
+            <BsInstagram />
+          </button>
+          <button>
+            <BsLinkedin />
+          </button>
+        </div>
+      </div>
+    </HeaderLayout>
+  );
 }
 
-
 const HeaderLayout = styled.section`
-    font-family: 'Proxima Nova';
-    font-style: normal;
-    
-    .categoryBadge{
-        background: #f8d7da;
-        border-radius: 3px;
-        color: var(--dark--red);
-        padding: .4rem .5rem;
-        font-size: 12px;
-        font-family: 'Proxima Nova';
-    }
+  font-family: "Proxima Nova";
+  font-style: normal;
 
-    h1{
-        margin: 1rem 0;
-        font-size: 40px;
-        line-height: 43px;
-        text-transform: uppercase;
-        color: var(--violet);
-    }
+  .categoryBadge {
+    background: #f8d7da;
+    border-radius: 3px;
+    color: var(--dark--red);
+    padding: 0.4rem 0.5rem;
+    font-size: 12px;
+    font-family: "Proxima Nova";
+  }
 
-    .socials{
-        margin-top: 1rem;
-        width: 250px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+  h1 {
+    margin: 1rem 0;
+    font-size: 40px;
+    line-height: 43px;
+    text-transform: uppercase;
+    color: var(--violet);
+  }
 
-        button {
-            background-color: transparent;
-            border-radius: 50%;
-            color: var(--violet);
-            font-size: 1.8rem;
-        }
-    }
+  .socials {
+    margin-top: 1rem;
+    width: 250px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-    @media (max-width: 767px){
-        width: 100%;
-        padding: 0 .5rem;
+    button {
+      background-color: transparent;
+      border-radius: 50%;
+      color: var(--violet);
+      font-size: 1.8rem;
     }
+  }
+
+  @media (max-width: 767px) {
+    width: 100%;
+    padding: 0 0.5rem;
+  }
 `;
