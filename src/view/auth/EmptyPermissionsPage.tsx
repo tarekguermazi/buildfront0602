4890ac08e963dd69268fcd3c452cf93b355b8d45
SalmaTalbi <salma.talbi@tadeco-group.tn>
2022-12-16 16:01:15 +0100

    Update EmptyPermissionsPage.tsx

--- src/view/auth/EmptyPermissionsPage.tsx
+++ src/view/auth/EmptyPermissionsPage.tsx
@@ -15,18 +15,18 @@ function EmptyPermissionsPage() {
     nav?.classList.add("abosulte");
   });
   return (
-    <div className='emptypermission'>
+    <div className="emptypermission">
       <div>
         <div>
           <i
-            className='fa-solid fa-xmark'
+            className="fa-solid fa-xmark"
             style={{ fontSize: 50, color: "#2B2840", float: "right" }}
             onClick={doSignout}
           />
         </div>
-        <div className='content__message'>
-          <div className='title__message'>Hi ! Mootassam</div>
-          <div className='detaill__message'>
+        <div className="content__message">
+          {/* <div className='title__message'>Hi ! Mootassam</div> */}
+          <div className="detaill__message">
             {i18n("auth.emptyPermissions.message")}
           </div>
         </div>
