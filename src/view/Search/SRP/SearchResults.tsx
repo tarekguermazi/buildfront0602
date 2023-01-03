17b624c6b188a2c8e0f4b7ae8a8b47a463add297
Mootassem <mootassem@tadeco-group.tn>
2023-01-03 16:50:03 +0100

    responsive search items

--- src/view/Search/SRP/SearchResults.tsx
+++ src/view/Search/SRP/SearchResults.tsx
@@ -11,50 +11,50 @@ import { i18n } from "src/i18n";
 import Breadcrumb from "src/view/shared/Breadcrumb";
 
 export default function SearchResults({ location }) {
-  // GLOBAL STATE
-  const { setIsLoading, SRP, setSRP, searchString, setcurrentPageIndex } =
-    useContext(SearchContext);
+	// GLOBAL STATE
+	const { setIsLoading, SRP, setSRP, searchString, setcurrentPageIndex } =
+		useContext(SearchContext);
 
-  const searchPublications = (SEARCH_STRING: string) => {
-    setIsLoading(true);
-    SearchService.getSearchResultsForPublicationsBasedOnSearchString(
-      SEARCH_STRING,
-      0
-    ).then((res) => {
-      setcurrentPageIndex(0);
-      setSRP(res);
-      setIsLoading(false);
-    });
-  };
-  // HANDLING SUB FILTER (date / name)
-  // const handleSubFilter = (event) => {
-  //   setIsLoading(true);
-  //   setSRP([]);
-  //   const FILTER_STRING = `&orderBy=[${event.target.value}]`;
-  //   // redo the search using chosen option
-  //   SearchService.getSearchResultsForPublicationsBasedOnSearchString(
-  //     searchString,
-  //     0
-  //   )
-  //     .then((res) => {
-  //       setSRP((SRP) => SRP.concat(res));
-  //       setIsLoading(false);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     });
-  // };
-  useEffect(() => {
-    searchPublications(location.HOME_SEARCH_STRING);
-  }, []);
-  let currentPostsToShow: any[] = [];
-  if (SRP.length >= 1)
-    if (SRP[0].rows.length >= 1) currentPostsToShow = SRP[0].rows;
+	const searchPublications = (SEARCH_STRING: string) => {
+		setIsLoading(true);
+		SearchService.getSearchResultsForPublicationsBasedOnSearchString(
+			SEARCH_STRING,
+			0,
+		).then((res) => {
+			setcurrentPageIndex(0);
+			setSRP(res);
+			setIsLoading(false);
+		});
+	};
+	// HANDLING SUB FILTER (date / name)
+	// const handleSubFilter = (event) => {
+	//   setIsLoading(true);
+	//   setSRP([]);
+	//   const FILTER_STRING = `&orderBy=[${event.target.value}]`;
+	//   // redo the search using chosen option
+	//   SearchService.getSearchResultsForPublicationsBasedOnSearchString(
+	//     searchString,
+	//     0
+	//   )
+	//     .then((res) => {
+	//       setSRP((SRP) => SRP.concat(res));
+	//       setIsLoading(false);
+	//     })
+	//     .catch((err) => {
+	//       console.error(err);
+	//     });
+	// };
+	useEffect(() => {
+		searchPublications(location.HOME_SEARCH_STRING);
+	}, []);
+	let currentPostsToShow: any[] = [];
+	if (SRP.length >= 1)
+		if (SRP[0].rows.length >= 1) currentPostsToShow = SRP[0].rows;
 
-  return (
-    <SRPLayout>
-      {/* SUB FILTER AND SRP METADA */}
-      {/* <SRPHeader>
+	return (
+		<SRPLayout>
+			{/* SUB FILTER AND SRP METADA */}
+			{/* <SRPHeader>
         {SRP.length >= 1 && (
           <>
             <div>
@@ -78,59 +78,15 @@ export default function SearchResults({ location }) {
         )}
       </SRPHeader> */}
 
-      {/* RESULT CONTENT DISPLAYED IN ARRAY FORMAT */}
-      <SRPTable currentPostsToShow={SRP} />
+			{/* RESULT CONTENT DISPLAYED IN ARRAY FORMAT */}
+			<SRPTable currentPostsToShow={SRP} />
 
-      {/* FOOTER WITH PAGINATION ENABLED */}
-      {/* <SRPFooter totalPosts={SRP.length >= 1 ? SRP[0].count : 0} /> */}
-    </SRPLayout>
-  );
+			{/* FOOTER WITH PAGINATION ENABLED */}
+			{/* <SRPFooter totalPosts={SRP.length >= 1 ? SRP[0].count : 0} /> */}
+		</SRPLayout>
+	);
 }
 
 const SRPLayout = styled.div`
-  margin-top: 1rem;
-`;
-
-const SRPHeader = styled.div`
-  width: 100%;
-  padding: 1rem 0;
-  font-size: 14px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-
-  div {
-    strong {
-      margin-right: 0.4rem;
-    }
-    span,
-    strong {
-      font-family: "Proxima Nova";
-      font-style: normal;
-      font-weight: 400;
-      font-size: 14px;
-      margin-right: 0.5rem;
-    }
-  }
-
-  #filterContainer {
-    span {
-      font-family: "Proxima Nova";
-      font-style: normal;
-      font-weight: 400;
-      font-size: 14px;
-      margin-right: 0.5rem;
-    }
-
-    select {
-      padding: 0.5rem;
-      font-family: "Proxima Nova";
-      font-style: normal;
-      font-weight: 700;
-      font-size: 14px;
-      line-height: 17px;
-      color: #2b2840;
-    }
-  }
+	margin-top: 1rem;
 `;
--- src/view/Search/Search.tsx
+++ src/view/Search/Search.tsx
@@ -8,48 +8,50 @@ import SearchResults from "./SRP/SearchResults";
 // OTHER
 
 export default function Search({ location }) {
-  // GLOBAL STATE
-  const [isLoading, setIsLoading] = useState(false);
-  const [searchString, setSearchString] = useState(location.HOME_SEARCH_STRING);
-  const [SRP, setSRP] = useState([]);
-  const [publicationFilter, setpublicationFilter] = useState("autre");
-  // PAGINATION LOGIC
-  console.log("first ", location);
-  const [currentPageIndex, setcurrentPageIndex] = useState(0);
-  const [numberOfPostsPerPage, setnumberOfPostsPerPage] = useState(5);
+	// GLOBAL STATE
+	const [isLoading, setIsLoading] = useState(false);
+	const [searchString, setSearchString] = useState(location.HOME_SEARCH_STRING);
+	const [SRP, setSRP] = useState([]);
+	const [publicationFilter, setpublicationFilter] = useState("autre");
+	// PAGINATION LOGIC
+	console.log("first ", location);
+	const [currentPageIndex, setcurrentPageIndex] = useState(0);
+	const [numberOfPostsPerPage, setnumberOfPostsPerPage] = useState(5);
 
-  return (
-    <SearchContext.Provider
-      value={{
-        isLoading,
-        setIsLoading,
-        SRP,
-        setSRP,
-        searchString,
-        setSearchString,
-        publicationFilter,
-        setpublicationFilter,
-        currentPageIndex,
-        setcurrentPageIndex,
-        numberOfPostsPerPage,
-        setnumberOfPostsPerPage,
-      }}
-    >
-      <SearchLayout>
-        <section className="wideContent">
-          <SearchResults location={location} />
-        </section>
-      </SearchLayout>
-    </SearchContext.Provider>
-  );
+	return (
+		<SearchContext.Provider
+			value={{
+				isLoading,
+				setIsLoading,
+				SRP,
+				setSRP,
+				searchString,
+				setSearchString,
+				publicationFilter,
+				setpublicationFilter,
+				currentPageIndex,
+				setcurrentPageIndex,
+				numberOfPostsPerPage,
+				setnumberOfPostsPerPage,
+			}}>
+			<SearchLayout>
+				<section className="wideContent">
+					<SearchResults location={location} />
+				</section>
+			</SearchLayout>
+		</SearchContext.Provider>
+	);
 }
 
 const SearchLayout = styled.section`
+	max-width: 1170px;
+	margin: auto;
 
-  max-width: 1170px;
-  margin: auto;
-  .wideContent {
-  display:flex; 
-  justify-content : flex-start;
-  }
+	@media (max-width: 1071px) {
+		margin: 150px 0 0 0;
+	}
+	.wideContent {
+		display: flex;
+		justify-content: flex-start;
+	}
 `;
