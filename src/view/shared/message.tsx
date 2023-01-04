9bb940404f4810f344022d995e83d407f6af4492
Mootassem <mootassem@tadeco-group.tn>
2023-01-04 14:21:37 +0100

    commit in routes.tsx

--- src/view/shared/message.tsx
+++ src/view/shared/message.tsx
@@ -2,15 +2,15 @@ import * as toastr from "toastr";
 import "toastr/build/toastr.css";
 
 export default class Message {
-  static success(arg) {
-    toastr.options.toastClass = "toastr";
-    toastr.options.positionClass = "toast-bottom-left";
-    toastr.success(arg);
-  }
+	static success(arg) {
+		toastr.options.toastClass = "toastr";
+		toastr.options.positionClass = "toast-bottom-left";
+		toastr.success(arg);
+	}
 
-  static error(arg) {
-    toastr.options.toastClass = "toastr";
-    toastr.options.positionClass = "toast-bottom-left";
-    toastr.error(arg);
-  }
+	static error(arg) {
+		toastr.options.toastClass = "toastr";
+		toastr.options.positionClass = "toast-bottom-left";
+		toastr.error(arg);
+	}
 }
