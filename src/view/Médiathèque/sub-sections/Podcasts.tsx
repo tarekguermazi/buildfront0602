81695eb4b434289a53b3fa35358675a4c27bcda4
Mootassam <Mootassame@gmail.com>
2023-01-23 16:56:36 +0100

    add the soundCloud in the mediatique

--- src/view/Médiathèque/sub-sections/Podcasts.tsx
+++ src/view/Médiathèque/sub-sections/Podcasts.tsx
@@ -3,21 +3,21 @@ import GridContentLayout from "../styles/GridContentLayout";
 import AudioPlayer from "../shared/AudioPlayer";
 
 export default function Podcasts({ podcastList }) {
-  return (
-    <GridContentLayout>
-      <div className='archieve__header'>
-        <h2>Podcast</h2>
-        <div className='satestique__bar'></div>
-      </div>
-      <div style={{ marginBottom: "2.1875rem" }} />
-      <div className='grid'>
-        {podcastList?.map((pod) => {
-          return (
-            <AudioPlayer dataSource={pod} key={pod._id} />
-            // END OF MAP RETURN
-          );
-        })}
-      </div>
-    </GridContentLayout>
-  );
+	return (
+		<GridContentLayout>
+			<div className="archieve__header">
+				<h2>Podcast</h2>
+				<div className="satestique__bar"></div>
+			</div>
+			<div style={{ marginBottom: "2.1875rem" }} />
+			<div className="grid">
+				{podcastList?.map((pod) => {
+					return (
+						<AudioPlayer dataSource={pod} key={pod._id} />
+						// END OF MAP RETURN
+					);
+				})}
+			</div>
+		</GridContentLayout>
+	);
 }
