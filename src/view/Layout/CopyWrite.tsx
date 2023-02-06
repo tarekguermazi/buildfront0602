63bfb218edc06c994063f3837b5a5c3b2ee124ca
SalmaTalbi <salma.talbi@tadeco-group.tn>
2023-02-06 10:37:13 +0100

    arabic chabges

--- src/view/Layout/CopyWrite.tsx
+++ src/view/Layout/CopyWrite.tsx
@@ -3,27 +3,50 @@ import { i18n } from "../../i18n";
 import { Link } from "react-router-dom";
 
 function CopyWrite() {
+  const language = localStorage.getItem("language");
+
   return (
     <div className="footer__">
       <div className="footer__copywrite">
-        <div className="copywrite">
-          <div className="copywrite__right">
-            <ul>
-              <li>
-                <Link
-                  className="footerLink"
-                  to="/contact"
-                  style={{ color: "#fff" }}
-                >
-                  {i18n("menu.Contact")}
-                </Link>
-              </li>
-              <li>{i18n("common.condition_utilisation")}</li>
-              <li>{i18n("common.mention_legal")}</li>
-            </ul>
+        {language === "fr" ? (
+          <div className="copywrite">
+            <div className="copywrite__right">
+              <ul>
+                <li>
+                  <Link
+                    className="footerLink"
+                    to="/contact"
+                    style={{ color: "#fff" }}
+                  >
+                    {i18n("menu.Contact")}
+                  </Link>
+                </li>
+                <li>{i18n("common.condition_utilisation")}</li>
+                <li>{i18n("common.mention_legal")}</li>
+              </ul>
+            </div>
+            <div className="copywrite__left">{i18n("common.copyright")}</div>
           </div>
-          <div className="copywrite__left">{i18n("common.copyright")}</div>
-        </div>
+        ) : (
+          <div className="copywrite">
+            <div className="copywrite__left">
+              <ul>
+                <li>
+                  <Link
+                    className="footerLink"
+                    to="/contact"
+                    style={{ color: "#fff" }}
+                  >
+                    {i18n("common.Contact")}
+                  </Link>
+                </li>
+                <li>{i18n("common.condition_utilisation")}</li>
+                <li>{i18n("common.mention_legal")}</li>
+              </ul>
+            </div>
+            <div className="copywrite__left">{i18n("common.copyright")}</div>
+          </div>
+        )}
       </div>
     </div>
   );
